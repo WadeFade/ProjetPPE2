dcfbabce360e5a1d091979b0e190e9fabd42259f
mathg <math44100@yahoo.fr>
2020-01-07 18:19:17 +0100

    Ajout caroussel

--- class/controleur.class.php
+++ class/controleur.class.php
@@ -144,8 +144,31 @@ class controleur {
 		</div>
 		';
 		return $retour;
-	}	
-	
+	}
+	public function retourne_caroussel() {
+            $retour = '<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block" src="./image/france/IMAGES%20CAROUSSEL%20RES/chateau-de-chenonceau.jpg" alt="First slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block" src="./image/france/IMAGES%20CAROUSSEL%20RES/notre-dame-de-paris-la-nuit.jpg" alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block" src="./image/france/IMAGES%20CAROUSSEL%20RES/Massif-des-Ecrins.jpg" alt="Third slide">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>';
+            return $retour;
+		}
 }
 
 ?>
